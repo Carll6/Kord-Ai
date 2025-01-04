10998fe1c9345da162299361edb7690a91a5cb12
Carll6 <carleykimberly283@gmail.com>
2025-01-04 16:33:47 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,28 +1,28 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: '', // put your session id between the ''
-  INVINCIBLE_MODE: false, // toogle invincible mode 
-  WORK_MODE: "Public",
+  OWNER_NUMBERS: "2349139657649", // your number with country code
+  OWNER_NAME: 'FHEMZY', // your owner name 
+  BOT_NAME: "ISHO", // your preferred bot name
+  SESSION_ID: 'kord_ai-0ZzbjV6D2yUt', // put your session id between the ''
+  INVINCIBLE_MODE: true, // toogle invincible mode 
+  WORK_MODE: "Private",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
   PREFIX: [".", "/", "!"],
-  WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
+  WELCOME_MESSAGE: "Hello! I'm FHEMZY, your friendly WhatsApp Editor.",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
   MAX_DOWNLOAD_SIZE: 100,
   AUTO_LIKE_STATUS: true,
-  AUTO_LIKE_EMOJI: '🍁',
+  AUTO_LIKE_EMOJI: '💻',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "Opay", // the name of your preferred bank
+    acc_number: "7039926259", // account number for your preferred bank
+    acc_name: "omotosoprecious" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -33,8 +33,8 @@ global.settings = {
   REJECT_CALLS: true,
   SEND_WELCOME_MESSAGE: false,
   WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
-  DELETE_WA_LINKS: false,
+  DELETE_WA_LINKS: true,
   BAD_WORD_FILTER: true,
-  BAD_WORDS: ['badword1', 'badword2', 'fuck', 'bitch', 'hentai'],
+  BAD_WORDS: ['ogunkill you', 'STFU', 'fuck', 'bitch', 'hentai'],
   ANTI_DELETE_ENABLED: true,
 };
